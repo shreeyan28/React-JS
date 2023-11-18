2ddde2dabeca061e209c162ac2d71b36cf970e68
shreeyan28 <sreean984@gmail.com>
2023-11-18 17:35:14 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className={styles.header}>
+       <center><h1 className={styles.mainTitle}>Lab - Client-side frameworks</h1></center>
       </header>
-    </div>
+      <div className={styles.app}>
+        <h2 className={styles.title}>Hello World</h2>
+      </div>
+    </>
   );
 }
 
